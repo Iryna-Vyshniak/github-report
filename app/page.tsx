786e911457025b41d7ed9f7f1ba0243f41845396
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-12-23 23:59:11 +0200

    feat: integrate report printing functionality on Home page
    - Added `Report` component to display printed output when triggered by the keyboard.
    - Introduced `isPrint` and `showReport` state to manage report visibility and animations.
    - Implemented `handlePrint` to toggle print state and trigger report display.
    - Utilized `useEffect` to show report when `isPrint` changes.
    - Enhanced printer section with visual separator and conditional rendering of the report.
    - Extended `Keyboard` component to handle `onPrint` callback for seamless integration.
    
    This update introduces print functionality to the Home page, enabling interactive report generation through the virtual keyboard.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,46 @@
 'use client';
 
 import Keyboard from '@/components/keyboard';
+import Report from '@/components/report';
 import Screen from '@/components/screen';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [text, setText] = useState<string>('');
+  const [isPrint, setIsPrint] = useState<boolean>(false);
+  const [showReport, setShowReport] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isPrint) {
+      setShowReport(true);
+    }
+  }, [isPrint]);
 
   const handleInput = (newText: string) => {
     setText(newText);
   };
+
+  const handlePrint = () => {
+    setIsPrint(!isPrint);
+  };
+
+  const handleAnimationEnd = () => {
+    if (!isPrint) {
+      setShowReport(false);
+    }
+  };
+
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-8 row-start-2 items-center'>
-        <div className='container mx-auto mt-0 w-full'>
+        <div className='container mx-auto mt-0 w-full max-w-[320px] s:max-w-full'>
           <Screen text={text} />
-          <Keyboard text={text} onInput={handleInput} />
+          <Keyboard text={text} onInput={handleInput} onPrint={handlePrint} />
+          <div className='mt-2 w-full printer relative'>
+            <div className='w-full bg-gray-600 dark:bg-gray-950 h-[2px]'></div>
+            {showReport && <Report isVisible={isPrint} onAnimationEnd={handleAnimationEnd} />}
+          </div>
         </div>
       </main>
       <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
