5bd4743e20c6f062823836e24836644d5c7edcb4
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-12-24 20:46:50 +0200

    style: Enhanced styles across multiple components to improve UI consistency and enhance user experience

--- components/report.tsx
+++ components/report.tsx
@@ -5,9 +5,9 @@ interface ReportProps {
 
 const Report = ({ isVisible, onAnimationEnd }: ReportProps) => {
   return (
-    <div className='absolute top-0 left-0 container-report p-2 w-full min-h-40 flex items-center justify-center'>
+    <div className='absolute top-0 left-0 transform translate-x-1/2 container-report p-2 w-full max-w-[18.75rem] min-h-40 flex items-center justify-center'>
       <div
-        className={`absolute top-0 left-0 transform translate-x-1/2 report min-h-40 ${
+        className={`absolute top-0 left-0 transform translate-x-1/2 report min-h-40 w-full ${
           isVisible ? 'report-print' : 'report-exit'
         }`}
         onAnimationEnd={!isVisible ? onAnimationEnd : undefined}
--- components/screen.tsx
+++ components/screen.tsx
@@ -10,8 +10,8 @@ const Screen: React.FC<ScreenProps> = ({ text }) => {
       value={text}
       readOnly
       rows={6}
-      className='screen mb-5 w-full max-w-[18.75rem] s:max-w-[28.75rem] sm:max-w-[36rem] md:max-w-[42rem] p-2  text-[10px] md:text-xs'
-      placeholder='Type here...'
+      className='screen mb-5 w-full max-w-[18.75rem] s:max-w-[28.75rem] sm:max-w-[36rem] md:max-w-[42rem] p-2  text-[10px] md:text-xs dark:text-slate-800 placeholder:text-slate-700 resize-none'
+      placeholder='Type a GitHub username here...'
     />
   );
 };
