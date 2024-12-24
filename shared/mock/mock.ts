3b6acfb4ec52725fad844e0baf4d9f0644b29083
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-12-25 00:35:05 +0200

    feat: add mock default user data for GitHub profile simulation
    - Created `defaultUser` object based on `LocalGithubUser` type to simulate GitHub user data.
    - Populated mock data with user details including `login`, `avatar`, `bio`, `location`, and social stats.
    - Included custom motivational `bio` and placeholder values for `company` and `blog`.
    - Simulated `location` with latitude and longitude to enhance testing scenarios.
    
    This update introduces a mock user profile for development and testing, enabling consistent UI behavior without live API calls.

---
+++ shared/mock/mock.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "@/shared/types/user";
+
+export const defaultUser: LocalGithubUser = {
+    "login": "Iryna V-N",
+    "avatar": "https://avatars.githubusercontent.com/u/111734415?s=400&u=7532c9eff203491cc02069116563b2bd05884f07&v=4",
+    "name": "Iryna-Vyshniak",
+    "company": "openToWork: true",
+    "blog": "Not yet",
+    "location": "{'location': { 'lat': 50.4501, 'lng': 30.5234 }",
+    "bio": "Believe in yourself, do what you love, and dare to challenge yourself for success!💫",
+    "twitter": '',
+    "public_gists": 6,
+    "public_repos": 92,
+    "followers": 142,
+    "following": 32,
+    "created": "2022-08-22T16:18:45Z",
+}
