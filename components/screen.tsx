cb85686184a1f9e3ab969cfa6e3918c7e552c210
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-01-11 14:32:39 +0200

    feat: enhance Screen component to allow user input for GitHub username

--- components/screen.tsx
+++ components/screen.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 interface ScreenProps {
   text: string;
+  onInput: (text: string) => void;
 }
 
-const Screen: React.FC<ScreenProps> = ({ text }) => {
+const Screen: React.FC<ScreenProps> = ({ text, onInput }) => {
   return (
     <textarea
       value={text}
-      readOnly
+      name='username'
+      onChange={(e) => onInput(e.target.value.trim())}
       rows={6}
-      className='screen mb-5 w-full max-w-[18.75rem] s:max-w-[28.75rem] sm:max-w-[36rem] md:max-w-[42rem] p-2  text-[10px] md:text-xs dark:text-slate-800 placeholder:text-slate-700 resize-none'
+      className='screen mb-5 w-full h-[8rem] max-w-[18.75rem] s:max-w-[28.75rem] sm:max-w-[36rem] md:max-w-[42rem] p-2 text-[10px] md:text-xs dark:text-slate-800 placeholder:text-slate-700 resize-none'
       placeholder='Type a GitHub username here...'
     />
   );
