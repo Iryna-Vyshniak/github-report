f08336d3cf7f18d5ded3c434852650d657156205
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-01-11 12:59:57 +0200

    feat: integrate Barcode component into Report for user data display

--- components/report.tsx
+++ components/report.tsx
@@ -1,17 +1,22 @@
+import Barcode from './barcode';
+
 interface ReportProps {
   isVisible: boolean;
   onAnimationEnd: () => void;
+  user: string;
 }
 
-const Report = ({ isVisible, onAnimationEnd }: ReportProps) => {
+const Report = ({ user, isVisible, onAnimationEnd }: ReportProps) => {
   return (
-    <div className='absolute top-0 left-0 transform translate-x-1/2 container-report p-2 w-full max-w-[18.75rem] min-h-40 flex items-center justify-center'>
+    <div className='absolute top-0 left-1/2 -translate-x-1/2 container-report p-2 w-full max-w-[18.75rem] min-h-40 flex items-center justify-center'>
       <div
-        className={`absolute top-0 left-0 transform translate-x-1/2 report min-h-40 w-full ${
+        className={`absolute top-0 left-0 -translate-x-1/2 report min-h-40 w-full ${
           isVisible ? 'report-print' : 'report-exit'
         }`}
         onAnimationEnd={!isVisible ? onAnimationEnd : undefined}
-      ></div>
+      >
+        <Barcode value={user} />
+      </div>
     </div>
   );
 };
