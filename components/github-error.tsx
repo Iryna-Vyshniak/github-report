e719bb8f18bfbebb32b45c37958b417b5ad7ad15
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-24 15:10:11 +0200

    feat: add GitHubError component to display animated error messages

---
+++ components/github-error.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+import { ERROR_MSG } from '@/shared/constants';
+
+export default function GitHubError() {
+  const [step, setStep] = useState<number>(0);
+
+  useEffect(() => {
+    if (step < ERROR_MSG.length) {
+      const timeout = setTimeout(() => setStep(step + 1), 1200);
+      return () => clearTimeout(timeout);
+    }
+  }, [step]);
+
+  return (
+    <pre className='text-red-800 text-center text-sm font-mono animate-pulse leading-tight'>
+      {`
+  ██████████████████████
+  █  ⚠️  SYSTEM ERROR  ⚠️  █
+  ██████████████████████
+
+  ${ERROR_MSG.slice(0, step + 1).join('\n')}
+      `}
+    </pre>
+  );
+}
