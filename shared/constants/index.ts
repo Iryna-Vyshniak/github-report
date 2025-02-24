71ff3067d1f7d3fefd3bf9849ea1c959deffa553
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-02-24 15:09:39 +0200

    feat: add loading steps and error messages constants for GitHub data retrieval

---
+++ shared/constants/index.ts
@@ -0,0 +1,16 @@
+export const LOADING_STEPS = [
+    "> Contacting GitHub satellites... ✅",
+    "> Establishing secure connection... 🔄",
+    "> Fetching repositories... 📡",
+    "> Processing commits... ⏳",
+    "> Generating final report... 📝",
+];
+
+export const ERROR_MSG = [
+    "> ERROR: Unable to retrieve user data. ❌",
+    "> Possible causes:",
+    "> - Username does not exist. 🛑",
+    "> - Rate limit exceeded. ⚠️",
+    "> - Unexpected GitHub response. 🔄",
+    "> Please check the username and try again.",
+];
